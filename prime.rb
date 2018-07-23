0b4454b5330ce4a6c4d1b154bc0a0fca6f290cd4
Forrest Benton <elliottbenton@gmail.com>
2018-07-23 22:08:58 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,13 +1,13 @@
 def prime?(integer)
   if integer <= 1 
     return false 
-  end
-  new_array = (2...integer).to_a
-  new_array.each do |num|
-    value = integer % num
-    if value == 0
-      return false
+  else
+    new_array = (2...integer).to_a
+    new_array.each do |num|
+      value = integer % num
+      if value == 0
+        return false
+      end
     end
-  end
-  return true
+    return true
 end
